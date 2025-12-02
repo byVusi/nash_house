d1e5cb4aa2609b182d420f1187035d6df46241de
Vusi Mathonsi <118907642+byVusi@users.noreply.github.com>
2025-12-02 17:17:21 +0200

    Update events list with new event entries

--- assets/js/Event.js
+++ assets/js/Event.js
@@ -8,9 +8,25 @@ class Event {
 	}
 }
 
-const events = [new Event("Remove Camp", "2025-01-11")];
+const events = [
+	// 2026 Easter
+	new Event("Remove Camp", "2026-01-16"),
+	new Event("Remove and New Boys' Induction", "2026-01-23"),
+	new Event("Inter-House Gala Pre-Events (Day 1)", "2026-01-26"),
+	new Event("Inter-House Gala Pre-Events (Day 2)", "2026-01-27"),
+	new Event("Inter-House Gala", "2026-01-29"),
+	new Event("Grobs", "2026-02-18"),
+	new Event("Half Term Starts", "2026-02-19"),
+	new Event("Half Term Ends", "2026-02-24"),
+	new Event("Inter-House Basketball", "2026-02-25"),
+	new Event("Senior Inter-House Tennis", "2026-03-07"),
+	new Event("Inter-House Music (Day 1)", "2026-03-30"),
+	new Event("Inter-House Music (Day 2)", "2026-03-31"),
+	new Event("School Closes", "2026-04-01")
+];
 
 export const sortedEvents = sortObjectsByDate(events, "date", true);
 
 // Special CSS styling applied when true
 sortedEvents[0].nextEvent = true;
+
