34e25f02374ad02a98153b7dba6fd1489527091e
Vusi Mathonsi <118907642+byVusi@users.noreply.github.com>
2025-12-04 13:18:28 +0200

    Update Media.js

--- assets/js/Media.js
+++ assets/js/Media.js
@@ -11,24 +11,8 @@ export class Media {
 export const media = [];
 
 // 2025 uploads
-for (let i = 0; i < 15; i++) {
-	media.push(
-		new Media("2025-12-04",
-		`assets/media/images/gallery/2025/2025-nash-${i}.jpeg`
-		)
-	);
-}
-
-// 2024 uploads
-for (let i = 0; i < 53; i++) {
-	media.push(
-		new Media(
-			"2024-12-09",
-			`assets/media/images/gallery/2024/2024-nash-${i}.jpg`
-		)
-	);
-}
 
 export const sortedMedia = sortObjectsByDate(media, "date", false);
+
 
 
