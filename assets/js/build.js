5d7bcc75d3459169c41b0799667f9dec0ce8e675
Vusi Mathonsi <118907642+byVusi@users.noreply.github.com>
2025-12-04 13:01:03 +0200

    Refactor build.js to improve card creation functions

--- assets/js/build.js
+++ assets/js/build.js
@@ -58,11 +58,12 @@ export function buildNewsCard(news) {
 	text.classList.add("card-text");
 	text.textContent =
 		news.text[0].trim().slice(-1) !== "."
-			? news.text[0].trim().slice(0, 120) + "..."
-			: news.text[0].trim().slice(0, 120) + "..";
+			? news.text[0].trim().slice(0, 140) + "..."
+			: news.text[0].trim().slice(0, 140) + "..";
 
 	link.classList.add("card-link");
 	link.setAttribute("href", news.linkHREF);
+	link.setAttribute("target", "_blank");
 	link.textContent = "Read more";
 
 	body.appendChild(title);
@@ -110,6 +111,7 @@ export function buildMedia(media) {
 
 	return content;
 }
+
 
 
 
