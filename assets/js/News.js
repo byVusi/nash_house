ded552ed4bf603806c1cb5146ecbb6beed6ec73d
byVusi <mathonsi@duck.com>
2025-12-05 08:09:29 +0200

    Fixed href on news link

--- assets/js/News.js
+++ assets/js/News.js
@@ -28,7 +28,7 @@ const news = [
 		[
 			"The words “A dream for many, a reality for few”, which are closely related to the house, captures exactly what it means to wear the iconic blue and white Nash colours. To be in Nash is not about the bare minimum - it is about living up to the standards, traditions and values that make the House not just any other House but Nash house. Many people dream of being a part of something so full and rich with heritage and culture, but only a few have the honour of calling themselves a Nashtonian. But as the saying goes “With great power (honour) comes great responsibility”: to give your best, to support your fellow Nashtonians and uphold the pride of the house in everything we do.",
 		],
-		"/assets/articles/2025_house_report.html",
+		"/nash_house/assets/articles/2025_house_report.html",
 		"2025-12-04",
 		"assets/media/images/gallery/2025/2025-nash-4.jpg",
 		"Mila Xego and Luke Brooks, Head of House and Deputy Head of House, respectively"
