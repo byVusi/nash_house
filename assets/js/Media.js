34fc2d4c7b878f973ca2a18e5280d87e3cbae90b
byVusi <mathonsi@duck.com>
2025-12-15 10:43:40 +0200

    Uploaded optimized images to gallery

--- assets/js/Media.js
+++ assets/js/Media.js
@@ -1,7 +1,11 @@
 import { sortObjectsByDate } from "./utils.js";
 
 export class Media {
-	constructor(date, imageSRC = "https://fakeimg.pl/600x400", imageALT = "") {
+	constructor(
+		date,
+		imageSRC = "./assets/media/images/placeholder.jpg",
+		imageALT = ""
+	) {
 		this.date = date;
 		this.imageSRC = imageSRC;
 		this.imageALT = imageALT;
@@ -12,10 +16,12 @@ export const media = [];
 
 // 2025 uploads
 for (let i = 0; i < 18; i++) {
-	media.push(new Media("2025-12-04", `assets/media/images/gallery/2025/2025-nash-${i}.jpg`));
+	media.push(
+		new Media(
+			"2025-12-04",
+			`assets/media/images/gallery/2025/2025-nash-${i}.jpg`
+		)
+	);
 }
 
 export const sortedMedia = sortObjectsByDate(media, "date", false);
-
-
-
